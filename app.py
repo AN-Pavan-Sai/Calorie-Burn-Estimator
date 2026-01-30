6b496cb30df0658427b18d67e60a273255f80f54
AN Pavan Sai <student102006@gmail.com>
2026-01-30 20:41:33 +0530

    imported other modules

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 import streamlit as st
 import pandas as pd
+import plotly.graph_objects as go
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import r2_score
